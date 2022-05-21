51923881ca26584a64528919a3f0ee5a8d8be705
Arcueid <dathemon@qualium-systems.com>
2022-05-21 17:58:39 +0200

    Adjust menu expand behaviour, remove unneded styles

--- docs/scripts/menu-scripts.js
+++ docs/scripts/menu-scripts.js
@@ -1,33 +1,35 @@
 ﻿let clicked;
 const expanded = "expanded";
-const menu = ".menu";
+const menu = ".menu-label";
 
 $(menu).mouseover(function (e) {
     reset();
     const target = $(e.target);
+    const f = target.attr("for");
+    const targetFor = $(`#${target.attr("for")}`)
+    targetFor.mouseleave(function () {
+        reset();
+    })
     if (!clicked) {
         if (!target.hasClass(expanded))
             target.addClass(expanded);
     }
-}).mouseout(function (e) {
-    if (!clicked) {
-        $(e.target).removeClass(expanded);
-    }
 }).click(function (e) {
     const target = $(e.target);
+    clicked = !clicked;
     if (clicked) {
         target.removeClass(expanded);
     } else {
         if (!target.hasClass(expanded))
             target.addClass(expanded);
     }
-    clicked = !clicked;
-});
+})
+
 
 $(document).mouseup(function (e) {
     const container = $(menu);
     const target = $(e.target);
-    
+
     if (!container.is(target) && container.has(target).length === 0) {
         container.removeClass(expanded);
     }
