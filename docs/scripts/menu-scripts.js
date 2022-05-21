bddd2afb3d2ef7faa2f2476a7aa9d989bfa10918
Arcueid <dathemon@qualium-systems.com>
2022-05-21 15:58:05 +0200

    Update style and documentation

---
+++ docs/scripts/menu-scripts.js
@@ -0,0 +1,39 @@
+﻿let clicked;
+const expanded = "expanded";
+const menu = ".menu";
+
+$(menu).mouseover(function (e) {
+    reset();
+    const target = $(e.target);
+    if (!clicked) {
+        if (!target.hasClass(expanded))
+            target.addClass(expanded);
+    }
+}).mouseout(function (e) {
+    if (!clicked) {
+        $(e.target).removeClass(expanded);
+    }
+}).click(function (e) {
+    const target = $(e.target);
+    if (clicked) {
+        target.removeClass(expanded);
+    } else {
+        if (!target.hasClass(expanded))
+            target.addClass(expanded);
+    }
+    clicked = !clicked;
+});
+
+$(document).mouseup(function (e) {
+    const container = $(menu);
+    const target = $(e.target);
+    
+    if (!container.is(target) && container.has(target).length === 0) {
+        container.removeClass(expanded);
+    }
+});
+
+function reset() {
+    $(menu).removeClass(expanded)
+    clicked = false;
+}
