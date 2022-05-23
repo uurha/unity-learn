2ea79e07224cd30204b2c346a4afda688111c7b9
uurha <uurh2man@gmail.com>
2022-05-23 02:18:03 +0200

    Add grades

--- docs/scripts/grade-scripts.js
+++ docs/scripts/grade-scripts.js
@@ -2,22 +2,10 @@ let grades = [
     'junior', 'middle', 'senior'
 ]
 
-$("body").on('DOMSubtreeModified', "#diagram", function () {
-    
-});
-
 $(window).load(function () {
     grades.forEach((value => {
-        const selector = `#grade-name="${value}"`;
-        const element = $(`div > :contains(${selector})`);
-        if (element == null) return;
-        element.attr('id', value);
-        let t = element.text();
-        t = t.replace(selector, '');
-        element.text(t);
-
-        var gradesMenu = $(`#grades ul`);
-        gradesMenu.append(`<li><a href="${value}" class="item control category">${value.toProperCase()}</a></li>`)
+        const gradesMenu = $(`#grades ul`);
+        gradesMenu.append(`<li><a href="#${value}" class="item control category">${value.toProperCase()}</a></li>`)
     }));
 });
 
@@ -26,3 +14,21 @@ String.prototype.toProperCase = function () {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 };
+
+$("body").on('DOMSubtreeModified', "#diagram", function () {
+    grades.forEach((value => {
+        const selector = `#grade-name="${value}"`;
+        const elements = $(`div > :contains(${selector}):last-child`);
+        var element = $(elements[elements.length - 1])
+        
+        if(element.attr("id") !== value) {
+            element.attr('id', value);
+            element.addClass("scroll-bottom")
+            element.addClass("blink-anchor")
+
+            let t = element.text();
+            t = t.replace(selector, '');
+            element.text(t);
+        }
+    }));
+});
